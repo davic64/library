25390f1ea8d07242cec71c3fefad76276c81f1a7
David Victoria <davidvictoria64@gmail.com>
2021-05-07 17:21:00 -0500

    feat: add book function adn add in dom

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log('Hello World');
+const CardsContainer = document.querySelector('.cards');
+
+let myLibrary = [];
+
+function Book(title, author, numPages, reading) {
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    this.reading = reading;
+}
+
+function addBook(title, author, numPages, reading) {
+    const book = new Book(title, author, numPages, reading);
+    myLibrary.push(book);
+}
+
+function enumerateBooks() {
+    myLibrary.map(book => {
+        const div = document.createElement('div');
+        div.textContent = book.title
+        CardsContainer.appendChild(div);
+    });
+}
+
+enumerateBooks();
