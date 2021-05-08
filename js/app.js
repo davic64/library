797b4326994a2033723916413093d5277d357d6d
David Victoria <davidvictoria64@gmail.com>
2021-05-07 19:25:32 -0500

    feat: add a book

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 const CardsContainer = document.querySelector('.cards');
+const Modal = document.querySelector('.modal');
+
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const numPages = document.querySelector("#numberPages");
+const reading = document.querySelector("#reading");
 
 let myLibrary = [];
 
@@ -11,15 +17,50 @@ function Book(title, author, numPages, reading) {
 
 function addBook(title, author, numPages, reading) {
     const book = new Book(title, author, numPages, reading);
-    myLibrary.push(book);
+    myLibrary.push({id: myLibrary.length, ...book});
 }
 
 function enumerateBooks() {
+    CardsContainer.textContent = "";
     myLibrary.map(book => {
         const div = document.createElement('div');
-        div.textContent = book.title
+        const span = document.createElement('span');
+
+
+        span.style.position = "absolute";
+        span.style.top = "10px";
+        span.style.right = "15px";
+        span.style.fontSize = "14px";
+        span.style.color ="#F35F5F"
+        span.style.cursor ="pointer"
+        span.addEventListener('click', () => alert(book.id));
+
+
+        span.textContent = "Delete"
+        div.className = "card"
+        div.innerHTML = `
+            <h2>${book.title}</h2>
+            <p>${book.author}</p>
+            <p>${book.numPages}</p>
+            <p>Reading: ${book.reading}</p>
+        `;
+        div.appendChild(span);
         CardsContainer.appendChild(div);
     });
+}
+
+function openModal() {
+    Modal.classList.toggle("closeModal");
+}
+
+function setBook() {
+    addBook(title.value, author.value, numPages.value, reading.checked);
+    openModal();
+    title.value = "";
+    author.value = "";
+    numPages.value = "";
+    reading.checked = false;
+    enumerateBooks();
 }
 
 enumerateBooks();
